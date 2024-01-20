6209d7e345b80bdf0038924fa2646967c8d340af
Jonzwang <Jonathanzw5518@gmail.com>
2024-01-20 12:33:00 -0600

    add lesson1

---
+++ lesson1/greeting.py
@@ -0,0 +1,6 @@
+name = input("What's your name?\n")
+message = f"Hello {name}. Good to see you!"
+#message = "Hello " + name + ", Good to see you!"
+#message = " ".join(["Hello", name+",", "Good to see you!"])
+#message = "Hello %s, Good to see you!" %(name)
+print(message)
